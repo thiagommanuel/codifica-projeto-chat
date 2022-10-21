8fc42702d4e90b64a14172c8410355cbe729c657
thiagommanuel <115325763+thiagommanuel@users.noreply.github.com>
2022-10-21 20:14:55 -0300

    better, but still needs the JSON part.

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ function cliente1() {
     var messageone = document.getElementById("mensagemUsuario1").value;
     clientone.push(messageone)
     var novoli = document.createElement("li")
+    novoli.classList.add("mensagemUsuario1")
     novoli.innerHTML = messageone
-    novoli.className = "mensagemUsuario1"
     var lista = document.getElementById("listaDeMensagens")
-    lista.append(messageone)
-
-
+    lista.append(novoli)
 }
-//
+
+function cliente2() {
+    var messageone = document.getElementById("mensagemUsuario2").value;
+    clientone.push(messageone)
+    var novoli = document.createElement("li")
+    novoli.classList.add("mensagemUsuario2")
+    novoli.innerHTML = messageone
+    var lista = document.getElementById("listaDeMensagens")
+    lista.append(novoli)
+}
